bef71717b3fee34be58f46ac50a95ff87cea16db
bnanchen <nanchenbastian@gmail.com>
2018-03-20 19:10:06 +0100

    WIP Vector's Join

--- cs422-project1/src/main/java/ch/epfl/dias/ops/vector/Join.java
+++ cs422-project1/src/main/java/ch/epfl/dias/ops/vector/Join.java
@@ -5,6 +5,7 @@ import ch.epfl.dias.store.DataType;
 import ch.epfl.dias.store.column.DBColumn;
 import ch.epfl.dias.store.row.DBTuple;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,7 +38,41 @@ public class Join implements VectorOperator {
 	@Override
 	public DBColumn[] next() {
 		// TODO: Implement
-        
+        DBColumn[] rightColumns = rightChild.next();
+        DBColumn[] leftColumns = leftChild.next();
+        ArrayList<ArrayList<Object>> result = new ArrayList<>();
+        for (int i = 0; i < leftColumns.length+rightColumns.length-1; i++) {
+            ArrayList<Object> col = new ArrayList<>();
+            result.add(col);
+        }
+
+        while (!leftColumns[0].eof) {
+            HashMap<Object, ArrayList<Integer>> hashes = new HashMap<>();
+            int index = 0;
+                Object[] leftFields = leftColumns[leftFieldNo].column;
+                for (Object lf : leftFields) {
+                    if (hashes.get(lf) != null) {
+                        hashes.get(lf).add(index);
+                    } else {
+                        ArrayList<Integer> listIndices = new ArrayList<>();
+                        listIndices.add(index);
+                        hashes.put(lf, listIndices);
+                    }
+                    index++;
+                }
+            while(!rightColumns[0].eof) {
+                Object[] rightFields = rightColumns[rightFieldNo].column;
+                for (Object rf : rightFields) {
+                    if (hashes.get(rf) != null) {
+                        for (int i = 0; i < leftColumns.length; i++) {
+                            for (int idx : hashes.get(rf)) {
+                                result.get(i).add() // TODO I was here!
+                            }
+                        }
+                    }
+                }
+            }
+        }
 		return null;
 	}
 
